9fab7859c6d70cd5e2eb8c5927b8776924046acd
IbrahamJimenez <48891375+IbrahamJimenez@users.noreply.github.com>
2019-03-24 15:41:07 -0600

    v3

--- Flask.py
+++ Flask.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/')
 def helloIndex():
     return 'Hello World from Python Flask!'
 
-app.run()
+app.run('0.0.0.0')
